922b80571b4f5c638276c4f274fed9853b3e8917
Allan Lei <allanlei@helveticode.com>
2011-06-02 12:10:43 -0700

    update setup dist

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
 from distutils.core import setup
+from setuptools import find_packages
 import os
 import sys
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def find_packages_in(where, **kwargs):
+    return [where] + ['%s.%s' % (where, package) for package in find_packages(where=where, **kwargs)]
+
 setup(
     name = 'django-openauth',
-    version = '0.0.1',
-    url = 'https://github.com/allanlei/django-openauth',
+    version = '0.1',
     author = 'Allan Lei',
     author_email = 'allanlei@helveticode.com',
-    description = 'OpenID/OAuth for Django',
-    download_url = 'git://github.com/allanlei/django-openauth.git',
+    description = ('OpenID/OAuth for Django'),
     license = 'New BSD',
+    keywords = 'openid oauth django',
+    url = 'https://github.com/allanlei/django-openauth',
+    packages=find_packages_in('openauth'),
+    long_description=read('README'),
+    install_requires=[
+        'python-openid>=2.2.5',
+        'oauth2>=1.5.170',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
